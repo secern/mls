de39718996a98d839c5aabde142ffe44215a2ceb
secern <secern@163.com>
2015-06-30 15:10:03 +0800

    sdfesft

--- client.php
+++ client.php
@@ -10,13 +10,19 @@ $context = new ZMQContext();
 
 //  Socket to talk to server
 echo "Connecting to hello world server…\n";
+ob_flush();
+flush();
 $requester = new ZMQSocket($context, ZMQ::SOCKET_REQ);
 $requester->connect("tcp://192.168.1.79:5555");
 
 for ($request_nbr = 0; $request_nbr != 10; $request_nbr++) {
     printf ("Sending request %d…\n", $request_nbr);
+ob_flush();
+flush();
     $requester->send("Hello");
 
     $reply = $requester->recv();
     printf ("Received reply %d: [%s]\n", $request_nbr, $reply);
+ob_flush();
+flush();
 }
