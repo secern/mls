0dfe0d7c369801cd1ef61ccf1743c259010bd681
secern <secern@163.com>
2015-06-30 16:04:35 +0800

    sdrtf

--- client.php
+++ client.php
@@ -9,30 +9,28 @@
 
 ini_set("display_errors", true);
 
-$conn = new mysql_connect("localhost", "root", "123456");
-mysql_select_db("test", $conn);
-mysql_query("set names 'utf-8'");
-
 $context = new ZMQContext();
 
 //  Socket to talk to server
 echo "Connecting to hello world server…\n";
-ob_flush();
-flush();
+
 $requester = new ZMQSocket($context, ZMQ::SOCKET_REQ);
 $requester->connect("tcp://192.168.1.79:5555");
 
 for ($request_nbr = 0; $request_nbr != 10; $request_nbr++) {
     printf ("Sending request %d…\n", $request_nbr);
-ob_flush();
-flush();
+
     $requester->send("Hello");
-
-    mysql_query("insert into test.test(content) values('hello')");
-
+myLog("abc");
     $reply = $requester->recv();
-    mysql_query("insert into test.test(content) values('reply')");
+myLog("abcd");
     printf ("Received reply %d: [%s]\n", $request_nbr, $reply);
-ob_flush();
-flush();
+}
+
+function myLog($content, $isObj = false){
+    if($isObj){
+        $content = serialize($content);
+    }
+    $logTxt = fopen(dirname(__FILE__)."/log.txt", "w");
+    fwrite($logTxt, $content."\n");
 }
